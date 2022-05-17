9efe321c846934c1c4b55d9af4725a0d4fb01da1
maxton135 <maxton135@gmail.com>
2022-05-17 14:44:07 -0700

    instruction pop up + base page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-document.getElementById('start-button').onclick = function() {
-  location.href = "play.js";
+let instructions = document.getElementById("instructions");
+
+function closeInstructions() {
+	instructions.classList.add("close-instructions");
 }
+
+const transition = document.getElementById("instructions");
+
+transition.addEventListener('transitionend', function(){
+	document.getElementById("instructions").style.display = "none";
+})
