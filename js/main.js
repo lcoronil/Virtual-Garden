582dbf77c2d5260fa791f4d0799245f630b0eea3
maxton135 <maxton135@gmail.com>
2022-06-02 19:47:19 -0700

    added more text

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/**
+ * Project:    Virtual Garden
+ * Authors:    Group 13 (Maxton lenox, Lee Coronilia, Alice Aiba, Claira Lind)
+ * Created:    March - June 2022
+ * Release:    https://lcoronil.github.io/Virtual-Garden/
+ *
+ **/
+
+// Intruction pop functions
 let instructions = document.getElementById("instructions");
 
 function closeInstructions() {
@@ -10,15 +19,19 @@ transition.addEventListener('transitionend', function(){
 	document.getElementById("instructions").style.display = "none";
 })
 
-// State of the pot
+// State of the pot variables
 var flowerType = "";
 var growthStage = 0;
 var potIsEmpty = true;
 var potIsReady = false;
+
+// Image srcs for each plant stage
 const begoniaStages = ["Beginiaseed.png", "Begoniasprout.png", "Begoniastage2.png", "Begoniastage3.png", "Begoniastage4.png", "Begoniaflower.png"]
 const songbirdStages = ["Songbird_Seed.png", "Songbird_Stage_One.png", "Songbird_Stage_Two.png", "Songbird_Stage_Three.png", "Songbird_Stage_Four.png", "Songbird.png"]
 const sunflowerStages = ["Sunflower_Seed.png", "Sunflower_Stage_One.png", "Sunflower_Stage_Two.png", "Sunflower_Stage_3.png", "Sunflower_Stage_Four_.png", "Sunflower.png"]
-const zinniaStages = ["Zinnia_seed.png", "Zinnia_Stage_One.png", "Zinnia_Stage_Two.png"]
+const zinniaStages = ["Zinnia_seed.png", "Zinnia_Stage_One.png", "Zinnia_Stage_Two.png", "zinniastage_3.png", "Zinnia_stage_4.png", "zinnia.png"]
+
+// Text displays for each plant
 const sunflowerText = [
 	"- Plant seed 2 to 4 inches deep in soil, 12 to 24 inches apart from one another.\r\n\r\n- There are many different looking sunflower seeds because there are over 70 varieties.\r\n\r\n- You typically want to wait to plant the seeds after the last frost of the year so the cold does not kill the seed.\r\n\r\n- Water it each day and see its progress!",
 	"Stage 1 text insert here",
@@ -28,20 +41,20 @@ const sunflowerText = [
 	"Final stage text insert here"
 ]
 const begoniaText = [
-	"Plant seed info text",
-	"Stage 1 text insert here",
-	"Stage 2 text insert here",
-	"Stage 3 text insert here",
-	"Stage 4 text insert here",
-	"Final stage text insert here"
+	"- Begonia seeds are as small as 4-15×2-9mm so unlike other plants, you can not place each seed individually. \r\n\r\n- Just sprinkle on top of a light, well-drained soil and wait till it germinates. The germination ends faster under warm conditions for about 70-80 degrees fahrenheit and sunlight isn't required.",
+	"- Make sure to space each pot of begonia by 6-12inches. \r\n\r\n- Wax begonias need full sunlight for growth but only partial sunlight is enough if the heat is intense. Too much sun heat can be stressful for begonia so make sure to provide a shade in that case. ",
+	"- Keep the temperature above 60 degrees Fahrenheit. \r\n\r\n- The wax begonia loves humidity for it being native to Central and South America. \r\n\r\n- Wax begonia can also be houseplants during winter if you can provide humidity. This can be done by placing the planting pot on top of a tray filled with water and pebbles so that the water the tray holds will eventually evaporate around the begonia. ",
+	"- Wax begonia is a summer flower that grows well in continually damp soil. \r\n\r\n- Some tips are to mix the soil with organic matter so that the flower is fed with nutrients. \r\n\r\n- Another tip is to provide fertilizer every month, during the growing season. But since the fertilizer is being used constantly, 10-10-10 would be enough range for a fertilizer. ",
+	"- Some problems seen in wax begonia are the flower rotting and spoiling. The plant will start rotting if you over hydrate the plant. Decrease the amount of water, though it should still be enough to keep the soil damp. The plant is spoiled if you leave deadhead flowers. ",
+	"- The grown wax begonia ranges from .5 to 1 feet tall and .5-1.5feet wide. \r\n\r\n- The entire lifespan of the plant is two to three years and a flower can last six months the longest. \r\n\r\n- Most will bloom a bright red or reddish pink flowers."
 ]
 const songbirdText = [
-	"Plant seed info text",
-	"Stage 1 text insert here",
-	"Stage 2 text insert here",
-	"Stage 3 text insert here",
-	"Stage 4 text insert here",
-	"Final stage text insert here"
+	"- Gently press in the columbine seeds on well-drained soil with light soil coverings to provide enough sunlight in order to germinate.\r\n\r\n- The best season to plant is in spring to early fall.",
+	"- Songbird Columbine is a herbaceous perennial. They grow best under perfect sunlight and slightly acidic soil. To adjust the best sunlight and temperature, the plant should be placed under shade if the sun is too hot and placed under full sun if the weather is often cool.",
+	"- A well drained soil must keep moist and never dry, though you should also not flood with water, making it swampy. \r\n\r\n- Rather than  a heavy clay like substance, the soil should be sandy and loamy, allowing hydration control of Columbine.",
+	"- Make sure to mulch your Columbine's soil! \r\n\r\n- Mulching is to cover the plant with an extra layer of soil on top of the old so that we can keep the moist environment and prevent  soil erosion.",
+	"- If the leaves turn yellow, it's a sign of not enough water! \r\n\r\n- Make sure to give enough water that the soil is always moist and also check if the soil is a good water holder or not. \r\n\r\n- Another bad symptom is leaves whitening, and that is the appearance of mildew. Treatment for this problem will be fungicide and if you can apply before the spread increase, your Columbine should go back healthy. ",
+	"- Songbird columbine at flower stage is about 6-9 inches tall and it blooms for about 4 weeks. \r\n\r\n- After blooming seasons, cut the plants down to the ground leaving some planting space. They are self-seeding wildflowers so a new Columbine will be planted naturally if you didn't collect the seeds."
 ]
 const zinniaText = [
 	"- Zinnias are able to adapt to most soil conditions, but the ideal soil is rich in organic matter and well-draining.\r\n\r\n- It is recommended to grow zinnia seeds right in the garden bed, they do not like to be transplanted.\r\n\r\n- From the seed, they'll grow quickly.\r\n\r\n- Sow zinnia seeds quarter-inch deep. After planting it will take several weeks to months before blooms appear.\r\n\r\n- When seedlings reach 3 inches tall, you must maximize their air circulation so they're 6-18 inches apart. It reduces the development of powdery mildew.",
@@ -51,7 +64,8 @@ const zinniaText = [
 	"After zinnias flower, cut off old flowers (a process called 'deadheading') to allow more flowers to form.",
 	"- Zinnias generally take 60-70 days to bloom. \r\n\r\n- Zinnias are annuals and will die in the frost of fall. To reseed them let the last flowers mature fully and scatter their seeds. \r\n\r\n"
 ]
-// update Pot function
+
+// update Pot function depending on current pot variables
 function updatePot() {
 	console.log("The status of the pot is as follows:")
 	console.log("  flowerType: " + flowerType)
@@ -59,6 +73,7 @@ function updatePot() {
 	console.log("  potIsEmpty: ", potIsEmpty)
 	console.log("  potIsReady: ", potIsReady)
 	var imgsrc = "img/";
+
 	if (flowerType == "songbird" && growthStage >= 0 && growthStage <= 5 ) {
 		imgsrc += songbirdStages[growthStage]
 		document.getElementById("potimg").src = imgsrc;
@@ -72,6 +87,7 @@ function updatePot() {
 		}
 		console.log(imgsrc);
 	}
+
 	else if (flowerType == "sunflower" && growthStage >= 0 && growthStage <= 5) {
 		imgsrc += sunflowerStages[growthStage]
 		document.getElementById("potimg").src = imgsrc;
@@ -85,6 +101,7 @@ function updatePot() {
 		}
 		console.log(imgsrc);
 	}
+
 	else if (flowerType == "begonia" && growthStage >= 0 && growthStage <= 5) {
 		imgsrc += begoniaStages[growthStage]
 		document.getElementById("potimg").src = imgsrc;
@@ -98,6 +115,7 @@ function updatePot() {
 		}
 		console.log(imgsrc);
 	}
+
 	else if (flowerType == "zinnia" && growthStage >= 0 && growthStage <=5) {
 		imgsrc += zinniaStages[growthStage]
 		document.getElementById("potimg").src = imgsrc;
@@ -114,11 +132,12 @@ function updatePot() {
 }
 
 // Dragging functions below
+
 const draggables = document.querySelectorAll(".draggable");
 const potdiv = document.getElementById("pot");
 const text = document.getElementById("text-display");
 
-
+// Drag start event listener for every tool/seed
 draggables.forEach(draggable => {
 	draggable.addEventListener('dragstart', ev => {
 		//text.textContent = "Drop to pot";
@@ -131,19 +150,20 @@ draggables.forEach(draggable => {
 	})
 })
 
-
+// Pot div dragover event listener
 potdiv.addEventListener('dragover', ev => {
 	//text.textContent = "Now drop!";
 	ev.preventDefault();
 	console.log('drag over');
 })
 
-
+// Pot div drop event listener
 potdiv.addEventListener('drop', ev => {
 	ev.preventDefault();
 	console.log("dropped");
 	var droppedData = ev.dataTransfer.getData("text");
 
+	// Check dropped object id
 	if (droppedData == "watercanimg") {
 		console.log("dropped img is watercan");
 		if (!potIsEmpty && potIsReady) {
@@ -232,24 +252,3 @@ potdiv.addEventListener('drop', ev => {
 	}
 
 })
-
-// still testing below
-/*
-// ensures this works for some older browsers
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-
-// the element you want to observe. change the selector to fit your use case
-const pimg = document.getElementById("potimg");
-
-new MutationObserver(function onSrcChange(){
-  // src attribute just changed!!! put code here
-	if (growthStage == 0) {
-		console.log("seed image just loaded!")
-		var val = 3;
-		$('#potimg').css({transition: 'transform 1s'});
-		$('#potimg').css({transform: 'scale(0.1) translateY(200%)'});
-		$('#potimg').css({transform: 'scale(0.1) translateY(200%)'});
-	}
-})
-  .observe(pimg,{attributes:true,attributeFilter:["src"]})
-*/
